b42c7e5697126418f55e043e585d4ac9aa3476c4
Muhammad Hasnain Ali <hasnainnali678@gmail.com>
2026-07-22 15:55:23 +0500

    first updated

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import First from "./src/component/first";
 
 const App = () => {
   return (
@@ -13,10 +14,11 @@ const App = () => {
       <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'white' }}>
         Hello Hasnain 🚀
       </Text>
-      <Text style={{fontSize: 40, color:'white', backgroundColor:'pink'}}>I am Mobile developer</Text>
+      <Text style={{ fontSize: 40, color: 'white', backgroundColor: 'pink' }}>I am Mobile developer</Text>
       <Button title='press Here' />
+      <First />
     </View>
   );
-}; 
+};                     
 
 export default App;
---
+++ src/component/first.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Button, Text, View} from "react-native";
+
+const First = () =>{
+    return(
+        <Text style={{ color: 'white' }}>Hello I am Here</Text>
+    );
+};
+export default First;
