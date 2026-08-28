d2e8b575a5ec9357422f3bd54c23698cbe027baf
Muhammad Hasnain Ali <hasnainnali678@gmail.com>
2026-08-28 17:02:35 +0500

    Async Storage is done

--- App.jsx
+++ App.jsx
@@ -1,91 +1,93 @@
 // Basic Concepts of react native
-// import React from 'react';
-// // import { View, Text, Button } from 'react-native';
-// // import First from "./src/component/first";
-// import { View } from 'react-native';
-// // import JSX from './src/component/JSX';
-// // import OnPress from './src/component/OnPress';
-// // import UseStateHook from './src/component/UseStateHook';
-// // import Styling from './src/component/Styling';
-// // import FlatListScreen from './src/component/FlatListScreen';
-// // import InputText from './src/component/InputText';
-// // import Props from './src/component/Props';
-// // import SectionListScreen from './src/component/SectionListScreen'
-// // import LoginForm from './src/component/LoginForm'
-// // import ContactListScreen from './src/screens/Contact/ContactListScreen'
-// // import Grid from './src/component/Grid'
-// // import ClassComponent from './src/component/ClassComponent'
-// // import UseEffectHook from './src/component/UseEffectHook'
-// // import UseEffectHookUnmountedPhase from './src/component/UseEffectHookUnmountedPhase'
-// // import HideShowToggle from './src/component/HideShowToggle'
-// // import StyleWithButton from './src/component/StyleWithButton'
-// // import Loader from './src/component/Loader'
-// // import PressableComponent from './src/component/PressableComponent'
-// // import StatusBarExample from './src/component/StatusBarExample'
-// // import UseRefHook from './src/component/UseRefHook'
-// // import ModalDialogBox from './src/component/ModalDialogBox'
-// // import AlertExample from './src/component/AlertExample'
-// // import GET_API from './src/component/GET_API'
-// // import POST_API from './src/component/POST_API'
-// // import PUT_API from './src/component/PUT_API'
-// // import PATCH_API from './src/component/PATCH_API'
-// // import DELETE_API from './src/component/DELETE_API'
-// // import GET_API_LIST from './src/component/GET_API_LIST'
-// // import PlatformExapmle from './src/component/PlatformExapmle'
+import React from 'react';
+// import { View, Text, Button } from 'react-native';
+// import First from "./src/component/first";
+import { View } from 'react-native';
+// import JSX from './src/component/JSX';
+// import OnPress from './src/component/OnPress';
+// import UseStateHook from './src/component/UseStateHook';
+// import Styling from './src/component/Styling';
+// import FlatListScreen from './src/component/FlatListScreen';
+// import InputText from './src/component/InputText';
+// import Props from './src/component/Props';
+// import SectionListScreen from './src/component/SectionListScreen'
+// import LoginForm from './src/component/LoginForm'
+// import ContactListScreen from './src/screens/Contact/ContactListScreen'
+// import Grid from './src/component/Grid'
+// import ClassComponent from './src/component/ClassComponent'
+// import UseEffectHook from './src/component/UseEffectHook'
+// import UseEffectHookUnmountedPhase from './src/component/UseEffectHookUnmountedPhase'
+// import HideShowToggle from './src/component/HideShowToggle'
+// import StyleWithButton from './src/component/StyleWithButton'
+// import Loader from './src/component/Loader'
+// import PressableComponent from './src/component/PressableComponent'
+// import StatusBarExample from './src/component/StatusBarExample'
+// import UseRefHook from './src/component/UseRefHook'
+// import ModalDialogBox from './src/component/ModalDialogBox'
+// import AlertExample from './src/component/AlertExample'
+// import GET_API from './src/component/GET_API'
+// import POST_API from './src/component/POST_API'
+// import PUT_API from './src/component/PUT_API'
+// import PATCH_API from './src/component/PATCH_API'
+// import DELETE_API from './src/component/DELETE_API'
+// import GET_API_LIST from './src/component/GET_API_LIST'
+// import PlatformExapmle from './src/component/PlatformExapmle'
 // import Search from './src/component/Search'
-// const App = () => {
-//   return (
-//     <View
-//     style={{flex:1}}
-//     // style={{
-//     //     flex: 1,
-//     //     justifyContent: 'center',
-//     //     alignItems: 'center',
-//     //   }}
-//       >
-//       {/* <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'white' }}>
-//         Hello Hasnain 🚀
-//         </Text>
-//         <Text style={{ fontSize: 40, color: 'white', backgroundColor: 'pink' }}>I am Mobile developer</Text>
-//         <Button title='press Here' />
-//         */}
-//       {/* <First />  */}
-//       {/* <JSX /> */}
-//       {/* <OnPress /> */}
-//       {/* <UseStateHook /> */}
-//       {/* <InputText  /> */}
-//       {/* <Styling /> */}
-//       {/* <FlatListScreen /> */}
-//       {/* <InputText /> */}
-//       {/* <Props /> */}
-//       {/* <SectionListScreen /> */}
-//       {/* <LoginForm /> */}
-//       {/* <ContactListScreen /> */}
-//       {/* <Grid /> */}
-//       {/* <ClassComponent /> */}
-//       {/* <UseEffectHook /> */}
-//       {/* <UseEffectHookUnmountedPhase /> */}
-//       {/* <HideShowToggle /> */}
-//       {/* <StyleWithButton /> */}
-//       {/* <Loader /> */}
-//       {/* <PressableComponent /> */}
-//       {/* <StatusBarExample /> */}
-//       {/* <UseRefHook /> */}
-//       {/* <ModalDialogBox /> */}
-//       {/* <AlertExample /> */}
-//       {/* <GET_API /> */}
-//       {/* <POST_API /> */}
-//       {/* <PUT_API/> */}
-//       {/* <PATCH_API/> */}
-//       {/* <DELETE_API /> */}
-//       {/* <GET_API_LIST /> */}
-//       {/* <PlatformExapmle /> */}
-//       <Search/>
-//     </View>
-//   );
-// };
+import AsyncStorageExample from './src/component/AsyncStorageExample'
+const App = () => {
+  return (
+    <View
+    style={{flex:1}}
+    // style={{
+    //     flex: 1,
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //   }}
+      >
+      {/* <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'white' }}>
+        Hello Hasnain 🚀
+        </Text>
+        <Text style={{ fontSize: 40, color: 'white', backgroundColor: 'pink' }}>I am Mobile developer</Text>
+        <Button title='press Here' />
+        */}
+      {/* <First />  */}
+      {/* <JSX /> */}
+      {/* <OnPress /> */}
+      {/* <UseStateHook /> */}
+      {/* <InputText  /> */}
+      {/* <Styling /> */}
+      {/* <FlatListScreen /> */}
+      {/* <InputText /> */}
+      {/* <Props /> */}
+      {/* <SectionListScreen /> */}
+      {/* <LoginForm /> */}
+      {/* <ContactListScreen /> */}
+      {/* <Grid /> */}
+      {/* <ClassComponent /> */}
+      {/* <UseEffectHook /> */}
+      {/* <UseEffectHookUnmountedPhase /> */}
+      {/* <HideShowToggle /> */}
+      {/* <StyleWithButton /> */}
+      {/* <Loader /> */}
+      {/* <PressableComponent /> */}
+      {/* <StatusBarExample /> */}
+      {/* <UseRefHook /> */}
+      {/* <ModalDialogBox /> */}
+      {/* <AlertExample /> */}
+      {/* <GET_API /> */}
+      {/* <POST_API /> */}
+      {/* <PUT_API/> */}
+      {/* <PATCH_API/> */}
+      {/* <DELETE_API /> */}
+      {/* <GET_API_LIST /> */}
+      {/* <PlatformExapmle /> */}
+      {/* <Search/> */}
+      <AsyncStorageExample />
+    </View>
+  );
+};
 
-// export default App;
+export default App;
 
 // ########## <starting navigation now> ##########
 
@@ -269,31 +271,31 @@
 
 // *************** <TOP TAB NAVIGATION> ***************
 
-import { View, Text } from 'react-native'
-import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
-import ChatsScreen from './src/Navigation-screens/TopTab-Navigation/ChatsScreen'
-import StatusScreen from './src/Navigation-screens/TopTab-Navigation/StatusScreen'
-import CallsScreen from './src/Navigation-screens/TopTab-Navigation/CallsScreen'
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import {NavigationContainer} from '@react-navigation/native'
+// import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+// import ChatsScreen from './src/Navigation-screens/TopTab-Navigation/ChatsScreen'
+// import StatusScreen from './src/Navigation-screens/TopTab-Navigation/StatusScreen'
+// import CallsScreen from './src/Navigation-screens/TopTab-Navigation/CallsScreen'
 
-const Tab = createMaterialTopTabNavigator(); 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarActiveTintColor:'#007bff',
-        tabBarInactiveTintColor:'gray',
-        tabBarStyle:{backgroundColor:'#fff'},
-        tabBarIndicatorStyle:{backgroundColor:'#007bff', height:3},
-        tabBarLabelStyle: {fontSize:16},
-      }}>
-        <Tab.Screen name='Chats' component={ChatsScreen}   />
-        <Tab.Screen name='Status' component={StatusScreen}  />
-        <Tab.Screen name='Calls' component={CallsScreen}  />
-      </Tab.Navigator>
-    </NavigationContainer>
-  )
-}
+// const Tab = createMaterialTopTabNavigator(); 
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator screenOptions={{
+//         tabBarActiveTintColor:'#007bff',
+//         tabBarInactiveTintColor:'gray',
+//         tabBarStyle:{backgroundColor:'#fff'},
+//         tabBarIndicatorStyle:{backgroundColor:'#007bff', height:3},
+//         tabBarLabelStyle: {fontSize:16},
+//       }}>
+//         <Tab.Screen name='Chats' component={ChatsScreen}   />
+//         <Tab.Screen name='Status' component={StatusScreen}  />
+//         <Tab.Screen name='Calls' component={CallsScreen}  />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   )
+// }
 
-export default App
+// export default App
